98c9dc82c3ba2bcc9b18302f2ed3918246e882f0
Syrinsecurity <=>
2020-07-22 07:54:32 +0100

    added docs

--- examples/customLogger/main.go
+++ examples/customLogger/main.go
@@ -10,14 +10,22 @@ type structExample struct {
 }
 
 func main() {
+	//Create a new custom logger
 	log := gologger.NewCustomLogger("./logs-", ".txt", 0)
 
 	//This will make the filename update every month for example: logs-Jul-2020.txt
 	gologger.SetNameConventionMonthYear(log)
+
+	//Start the logger service on another goroutine
 	go log.Service()
+
+	//Make sure to close the logger
 	defer log.Close()
 
-	log.Write("test", "1", "2", "3")
+	//write multipule values to the log with any data type
+	log.Write("test", 1, 2, 2)
+
+	//Convert any object to JSON and write it to the log
 	log.WriteJSON(structExample{
 		Feild1: "value1",
 		Feild2: "value2",
